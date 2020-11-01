2c8d8c8eeca724fc01fa4b5c216b898412a7cc82
Meepo <524545375@qq.com>
2020-11-01 22:26:43 +0800

    俄罗斯方块基本实现1

---
+++ Assets/blockController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blockController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody2D rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.velocity = new Vector2(0f, -speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
